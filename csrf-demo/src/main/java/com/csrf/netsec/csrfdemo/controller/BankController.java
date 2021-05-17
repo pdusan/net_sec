4f353c492a92d16b0395eccb5135648a71754bff
Dusan Popovic <a01651258@unet.univie.ac.at>
2021-05-17 13:44:07 +0200

    added balance subtraction

--- csrf-demo/src/main/java/com/csrf/netsec/csrfdemo/controller/BankController.java
+++ csrf-demo/src/main/java/com/csrf/netsec/csrfdemo/controller/BankController.java
@@ -1,21 +1,27 @@
 package com.csrf.netsec.csrfdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.csrf.netsec.csrfdemo.models.Customer;
 
 @Controller
-@RequestMapping("/account")
 public class BankController {
 	
+	Customer a = new Customer();
+	
+	@RequestMapping("/account") 
     @GetMapping
-    public String transferMoney() {
-        return "account";
-    }
+    public String display(Model model) {
+		model.addAttribute("balance", a.getBalance());
+		return "account";
+	}
     
-    @PostMapping("/transfer")
+    @PostMapping("/account/transfer")
     public String transfer() {
+    	a.removeMoney(100);
         return "success";
     }
 }
---
+++ csrf-demo/src/main/java/com/csrf/netsec/csrfdemo/models/Customer.java
@@ -0,0 +1,31 @@
+package com.csrf.netsec.csrfdemo.models;
+
+public class Customer {
+	private int balance;
+	private String name;
+	
+	public Customer() {
+		this.balance = 1000;
+		this.name = "John";
+	}
+
+	public int getBalance() {
+		return this.balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void removeMoney(int amount) {
+		this.balance -= amount;
+	}
+}
